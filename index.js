2e6f72947d53b8344e6919c63ab446e08866a3d5
ReksiGustio <reksiandika.business@gmail.com>
2024-06-09 22:32:56 +0700

    adding multer

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const express = require('express')
+const multer = require('multer')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const router = require('./routes')
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '/uploads')
+  },
+  filename: function (req, file, cb) {
+      var getFileExt = function(fileName){
+        var fileExt = fileName.split(".");
+        if (fileExt.length === 1 || ( fileExt[0] === "" && fileExt.length === 2)) {
+          return "";
+        }
+        return fileExt.pop();
+      }
+      cb(null, Date.now() + '.' + getFileExt(file.originalname))
+  }
+})
+
+
+const upload = multer( { storage } )
+const type = upload.single('profile_pic');
 const app = express()
 const port = 3000;
 
@@ -16,6 +36,11 @@ app.get('/', (req, res) => {
 })
 
 app.use('/', router)
+
+//send image
+app.post('/upload', type, function(req, res) {
+  res.json(req.file)
+})
 
 //start server
 app.listen(port, () => {
