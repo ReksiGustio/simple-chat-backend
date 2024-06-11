18141bb4c7d8b4e5b24f12bb8205ad0fea44bc90
ReksiGustio <reksiandika.business@gmail.com>
2024-06-11 22:10:55 +0700

    added send picture in message.

--- index.js
+++ index.js
@@ -22,9 +22,32 @@ const storage = multer.diskStorage({
   }
 })
 
+const messageStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/message')
+  },
+  filename: function (req, file, cb) {
+      const sender = req.params.sender
+      const receiver = req.params.receiver
+      const id = req.params.id
 
-const upload = multer( { storage } )
+      var getFileExt = function(fileName){
+        var fileExt = fileName.split(".");
+        if (fileExt.length === 1 || ( fileExt[0] === "" && fileExt.length === 2)) {
+          return "";
+        }
+        return fileExt.pop();
+      }
+      cb(null, String(sender) + '-' + String(receiver) + '-' + String(id) + '-' + file.fieldname + '.' + getFileExt(file.originalname))
+  }
+})
+
+const upload = multer( { storage: storage } )
+const uploadMessage = multer( { storage: messageStorage } )
+
 const type = upload.single('profile_pic');
+const messageType = uploadMessage.single('message_pic');
+
 const app = express()
 const port = 3000;
 
@@ -44,8 +67,16 @@ app.post('/upload/profile/:userName', type, function(req, res) {
   res.json(req.file)
 })
 
+//send message image
+app.post('/upload/message/:sender&:receiver&:id', messageType, function(req, res) {
+  res.json(req.file)
+})
+
 //fetch image
 app.use('/download/profile', express.static(__dirname + '/uploads/profile'))
+
+//fetch message image
+app.use('/download/message', express.static(__dirname + '/uploads/message'))
 
 //start server
 app.listen(port, () => {
